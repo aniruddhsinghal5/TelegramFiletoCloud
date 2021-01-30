811f881c034b5dbccca31f2542f082b3235c0292
Aniruddh Singhal <75318904+aniruddhsinghal5@users.noreply.github.com>
2021-01-30 19:35:08 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -37,7 +37,7 @@ except KeyError:
 
 
 class msg():
-    source = "\nsource: https://github.com/Abhijith-cloud/TelegramFiletoCloud"
-    start = "\n<b>This bot uploads telegram files to MixDrop.co,File.io.\nAdmin: @thankappan369</b>"
-    error = "something is went wrong\n{error} \ncontact admin @thankappan369"
+    source = "\nOwner: https://t.me/aniruddhsinghal5"
+    start = "\n<b>This bot uploads Telegram files to MixDrop.co,File.io."
+    error = "Something went wrong\n{error} \ncontact owner @aniruddhsinghal5"
     help = "Usage: <b>Send any file and the bot will upload it to MixDrop.co,File.io</b>"
